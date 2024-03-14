c92762152ea1e952eeab6454365e87118271f348
Joker <1574427203@qq.com>
2024-03-14 16:47:47 +0800

    会议页面提交

---
+++ ISDT 2024_files/main.js
@@ -0,0 +1,19 @@
+﻿var speed=30;
+var banner1=document.getElementById('banner1');
+var banner2=document.getElementById('banner2');
+var banner=document.getElementById('banner');
+//console.log("vivi");
+banner2.innerHTML=banner1.innerHTML;
+function marQuee(){
+	//console.log(banner2.offsetWidth-banner.scrollLeft);
+	if(banner2.offsetWidth-banner.scrollLeft<=0){
+		banner.scrollLeft-=banner1.offsetWidth;
+	}
+	else{
+		banner.scrollLeft++;
+	}
+	
+}
+var MyMar=setInterval(marQuee,speed)
+banner.onmouseover=function() {clearInterval(MyMar)}
+banner.onmouseout=function() {MyMar=setInterval(marQuee,speed)}
